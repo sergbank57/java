4fa63dd24a1944ea31b6254348ca84e529e20f3a
denis <79594203sa@gmail.com>
2022-12-22 20:09:04 +0100

    Initial commit

---
+++ src/less2/Main.java
@@ -0,0 +1,12 @@
+package less2;
+
+public class Main {
+    public static void main(String[] args) {
+        String imea1 ="kot";
+        String imea2 = "dva";
+        System.out.println(imea1 + "i pet" + ".");
+        System.out.println("dva i ses");
+        System.out.println("tri i" + imea1);
+
+    }
+}
